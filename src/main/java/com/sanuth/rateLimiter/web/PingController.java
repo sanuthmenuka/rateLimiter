9597cb71937993e63e66d872708e7be599dd4815
Menuka L.D.S <104838986+sanuthmenuka@users.noreply.github.com>
2026-02-10 18:05:00 +0530

    ping api

---
+++ src/main/java/com/sanuth/rateLimiter/web/PingController.java
@@ -0,0 +1,17 @@
+package com.sanuth.rateLimiter.web;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PingController {
+
+    @GetMapping("/ping")
+    public Map<String, String> ping() {
+        return Map.of("status", "ok");
+    }
+}
